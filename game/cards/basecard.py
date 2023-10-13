d839b98520054e5c7a5eaa7cb559bc333e618725
Murtaza Lightwala <murtaza.lightwala@gmail.com>
2023-10-13 06:19:54 +0530

    modified:   game/actions.py 	modified:   game/buff.py 	modified:   game/cards/basecard.py 	modified:   game/cards/fire.py 	modified:   game/game.py 	modified:   game/player.py 	modified:   game/slot.py 	modified:   rough.ipynb

--- game/cards/basecard.py
+++ game/cards/basecard.py
@@ -1,3 +1,6 @@
+from game.slot import Slot
+from game.actions import ApplyDebuff, BasicAttack
+
 class Deck:
     def __init__(self) -> None:
         pass
@@ -6,4 +9,61 @@ class Deck:
         pass
 
 class BaseCard:
-    pass
+    type:str
+    name:str
+    attack:int
+    life:int
+    mana_cost:int
+    state:str
+    slot: Slot
+    spell:bool = False
+    wall:bool = False
+
+    def __init__(self, slot, *args, **kwargs):
+        self.slot = slot
+        self.buffs_doer = set()
+        self.buffs_target = set()
+        self.state = "drafted"
+
+    def take_damage(self, damage, type, stage, *args, **kwargs):
+        self.life -= damage
+
+    def get_healed(self, health, stage, *args, **kwargs):
+        self.life += health
+
+    def start_of_round_actions(self, *args, **kwargs):
+        return []
+
+    def start_of_owner_turn_actions(self, *args, **kwargs):
+        return []
+    
+    def start_of_opponent_turn_actions(self, *args, **kwargs):
+        return []
+
+    def end_of_round_actions(self, *args, **kwargs):
+        return []
+
+    def end_of_owner_turn_actions(self, *args, **kwargs):
+        return []
+    
+    def end_of_opponent_turn_actions(self, *args, **kwargs):
+        return []
+
+    def summon_actions(self, *args, **kwargs):
+        return []
+
+    def death_actions(self, *args, **kwargs):
+        for buff in self.buffs:
+            yield ApplyDebuff(buff = buff, doer = self, target = buff.target, *args, **kwargs)
+
+    def turn_actions(self, *args, **kwargs):
+        opponent = self.slot.player.opponent
+        yield BasicAttack(damage = self.attack, doer = self, target = opponent.slots[self.slot.slot_id], *args, **kwargs)
+    
+
+class SpellMixin:
+    attack = 0
+    life = 0
+
+    def __init__(self, player, *args, **kwargs) -> None:
+        self.player = player
--- game/player.py
+++ game/player.py
@@ -17,6 +17,9 @@ class BasePlayer:
         self.mana_inc = {}
         self._life = 0
         self.deck = Deck()
+        self._game = None
+        self.buffs_doer = set()
+        self.buffs_target = set()
 
     def build_by_config(self, game_config, *args, **kwargs):
         game_config.configure_player(self)
@@ -29,6 +32,9 @@ class BasePlayer:
     def life(self, value):
         self._life = value
 
+    def get_mana(self, element, *args, **kwargs):
+        return self.mana.get(element, 0)
+
     def set_mana(self, element, value = None, *args, **kwargs):
         if value == None:
             value = self.mana.get(element, 0) + self.mana_inc.get(element, 0)
@@ -36,6 +42,12 @@ class BasePlayer:
 
     def decrease_mana(self, element, value = 0, *args, **kwargs):
         self.mana[element] -= value
+    
+    def increase_mana(self, element, value, *args, **kwargs):
+        self.mana[element] += value
+    
+    def get_mana_inc(self, element, *args, **kwargs):
+        return self.mana_inc.get(element, 0)
 
     def set_mana_inc(self, element, value = 1, *args, **kwargs):
         self.mana_inc[element] = value
@@ -49,18 +61,41 @@ class BasePlayer:
     def mana_drain(self, element, decrease, *args, **kwargs):
         self.mana[element] -= decrease
 
-    def mana_increase(self, element, increase, *args, **kwargs):
-        self.mana[element] += increase
-    
     def shuffle_deck(self, element, *args, **kwargs):
         self.deck.shuffle(element)
 
     def play_card(self, card_name, slot_id, *args, **kwargs):
         card_info = CardMap.get_card_info(card_name)
+        summon_actions = []
         if self.slots[slot_id].is_empty:
             if self.mana.get(card_info.get("type"))>=card_info.get("mana_cost"):
-                self.slots[slot_id].set_card(CardMap.get_card_by_name(card_name, player = self, slot = self.slots[slot_id], slot_id = slot_id))
+                card = CardMap.get_card_by_name(card_name, player = self, slot = self.slots[slot_id], slot_id = slot_id)
+                self.slots[slot_id].set_card(card)
                 self.decrease_mana(card_info.get("type"), card_info.get("mana_cost", 0))
+                summon_actions = [x for x in card.summon_actions()]
+        return summon_actions
+    
         
+    
+    @property
+    def game(self):
+        return self._game
+    
+    @game.setter
+    def set_game(self, game):
+        self._game = game
 
+    @property
+    def opponent(self):
+        if self.game is None:
+            return None
+        if self.game.player_1 == self:
+            return self.game.player_2
+        else:
+            return self.game.player_1
+        
+    def play_summon_effects(self, *args, **kwargs):
+        pass
+        
+    
 
